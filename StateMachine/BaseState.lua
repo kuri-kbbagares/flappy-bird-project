740bbde858faa7b904b753d7b69bdace06a43a75
shaina-espulgar <72699933+shaina-espulgar@users.noreply.github.com>
2020-12-11 17:50:15 +0800

    Added StateMachine
    For the Main Menu

---
+++ StateMachine/BaseState.lua
@@ -0,0 +1,11 @@
+--[[
+    Used as the Base class for all of our states
+]]
+
+BaseState = Class{}
+
+function BaseState:init() end
+function BaseState:enter() end
+function BaseState:exit() end
+function BaseState:update(dt) end
+function BaseState:render() end
---
+++ StateMachine/CountdownState.lua
@@ -0,0 +1,31 @@
+
+CountdownState = Class{__includes = BaseState}
+
+Countdown_Time = 0.75
+
+function CountdownState:init()
+    self.count = 3
+    self.timer = 0
+end
+
+--[[
+    When time goes to 0, PlayState will be executed
+]]
+
+function CountdownState:update(dt)
+    self.timer = self.timer + dt
+
+    if self.timer > Countdown_Time then
+        self.timer = self.timer % Countdown_Time
+        self.count = self.count - 1
+
+        if self.count == 0 then
+            gStateMachine:change('play')
+        end
+    end
+end
+
+function CountdownState:render()
+    love.graphics.setFont(hugeFont)
+    love.graphics.printf(tostring(self.count), 0, 120, VIRTUAL_WIDTH, 'center')
+end
