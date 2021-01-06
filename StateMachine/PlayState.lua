c5a4426b6dc1dba451877fc2531a30bc58f4ee6f
Danish Sibulo <72681403+dmdsibulo@users.noreply.github.com>
2021-01-06 11:39:19 +0800

    Update PlayState.lua

--- StateMachine/PlayState.lua
+++ StateMachine/PlayState.lua
@@ -4,7 +4,8 @@ function PlayState:init()
     self.bird = Birb()
     self.pipesTable = {}
     self.timer = 0
-
+    self.score = 0
+    
     self.limitY = -PIPE_HEIGHT + math.random(80) + 20
 end
 
@@ -22,7 +23,36 @@ function PlayState:update(dt)
       table.insert(self.pipesTable, PipePair(y))
       self.timer = 0
     end
-  
+    for k, pair in pairs(self.pipesTable) do
+        if not pair.scored then
+            if pair.x + PIPE_WIDTH < self.bird.x then
+                self.score = self.score + 1
+                pair.scored = true
+                sounds['score']:play()
+            end
+        end
+        pair:update(dt)
+    end
+
+    for k, pair in pairs(self.pipesTable) do
+        if pair.remove then
+            table.remove(self.pipesTable, k)
+        end
+    end
+
+    for k, pair in pairs(self.pipesTable) do
+        for l, pipe in pairs(pair.pipes) do
+            if self.bird:collides(pipe) then
+                sounds['explosion']:play()
+                sounds['hurt']:play()
+
+                gStateMachine:change('score', {
+                    score = self.score
+                })
+            end
+        end
+    end
+    
     birb:update(dt)
   
     for k, pair in pairs(self.pipesTable) do
@@ -43,15 +73,42 @@ function PlayState:update(dt)
     for k, pair in pairs(self.pipesTable) do
       for l, pipe in pairs(pair.pipes) do
         if self.bird:collides(pipe) then
-          -- place the new ScoreState file here to process the collision
+          sounds['explosion']:play()
+                sounds['hurt']:play()
+
+                gStateMachine:change('score', {
+                    score = self.score
+                })
         end
       end
     end
     
+   self.bird:update(dt)
+
+    if self.bird.y > VIRTUAL_HEIGHT_PUSH - 15 then
+        sounds['explosion']:play()
+        sounds['hurt']:play()
+
+        gStateMachine:change('score', {
+            score = self.score
+        })
+    end   
 end
 
 function PlayState:render()
     for k, pair in pairs(self.pipesTable) do
         pair:render()
     end
+    love.graphics.setFont(flappyFont)
+    love.graphics.print('Score: ' .. tostring(self.score), 8, 8)
+
+    self.bird:render()
+end
+
+function PlayState:enter()  
+    scrolling = true
+end
+
+function PlayState:exit()
+    scrolling = false
 end
