98e14dba80f829e2e9508d9b19e43d74765f9c7d
Danish Sibulo <72681403+dmdsibulo@users.noreply.github.com>
2021-01-06 11:42:00 +0800

    Update ScoreState.lua

--- StateMachine/ScoreState.lua
+++ StateMachine/ScoreState.lua
@@ -12,10 +12,10 @@ end
 
 function ScoreState:render()
     love.graphics.setFont(flappyFont)
-    love.graphics.printf('Oof! You lost!', 0, 64, VIRTUAL_WIDTH_PUSH, 'center') --adeed push
+    love.graphics.printf('Oof! You lost!', 0, 64, VIRTUAL_WIDTH_PUSH, 'center')
 
     love.graphics.setFont(mediumFont)
-    love.graphics.printf('Score: ' .. tostring(self.score), 0, 100, VIRTUAL_WIDTH_PUSH, 'center') --added push
+    love.graphics.printf('Score: ' .. tostring(self.score), 0, 100, VIRTUAL_WIDTH_PUSH, 'center')
 
-    love.graphics.printf('Press Enter to Play Again!', 0, 160, VIRTUAL_WIDTH_PUSH, 'center') --added push
+    love.graphics.printf('Press Enter to Play Again!', 0, 160, VIRTUAL_WIDTH_PUSH, 'center')
 end
